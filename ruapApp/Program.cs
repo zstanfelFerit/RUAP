23b63dfe2f37736cff01e9512ac1eab508ed8fbf
Zel <zstanfel@gmail.com>
2023-10-30 14:50:37 +0100

    VS code
    Added some VS code

---
+++ ruapApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ruapApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
