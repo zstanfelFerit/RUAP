4658ddd1c9333d3ae7342fdb83cf875f92fa895e
Zeljko Stanfel <zstanfel@gmail.com>
2024-11-04 08:50:50 +0100

    test commit

--- ruapApp/Program.cs
+++ ruapApp/Program.cs
@@ -6,7 +6,6 @@ namespace ruapApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World git!")
         }
-    }
-}
+    }}
